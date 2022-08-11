cb3dcc294b3f2b74d2649668c6451120f3b2d74c
Simon Damberg <damberg.simon00@gmail.com>
2022-08-11 16:24:33 +0200

    🚀 Added correct types to chart props

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,18 +2,56 @@ import {
     MouseEventHandler,
     TimeRangeChangeEventHandler,
     IChartApi,
+    SingleValueData,
+    CandlestickSeriesPartialOptions,
+    LineSeriesPartialOptions,
+    AreaSeriesPartialOptions,
+    BarSeriesPartialOptions,
+    HistogramSeriesPartialOptions,
+    OhlcData,
+    ChartOptions
 } from "lightweight-charts";
 import React, { FC } from "react";
 
+export interface ICandlestickSeries {
+    id: string;
+    data: OhlcData[];
+    options: CandlestickSeriesPartialOptions;
+}
+
+export interface ILineSeries {
+    id: string;
+    data: SingleValueData[];
+    options: LineSeriesPartialOptions;
+}
+
+export interface IAreaSeries {
+    id: string;
+    data: SingleValueData[];
+    options: AreaSeriesPartialOptions;
+}
+
+export interface IBarSeries {
+    id: string;
+    data: SingleValueData[];
+    options: BarSeriesPartialOptions;
+}
+
+export interface IHistogramSeries {
+    id: string;
+    data: SingleValueData[];
+    options: HistogramSeriesPartialOptions;
+}
+
 export interface IChart extends React.HTMLProps<FC> {
-    candlestickSeries?: Array<any>;
-    lineSeries?: Array<any>;
-    areaSeries?: Array<any>;
-    barSeries?: Array<any>;
-    histogramSeries?: Array<any>;
+    candlestickSeries?: Array<ICandlestickSeries>;
+    lineSeries?: Array<ILineSeries>;
+    areaSeries?: Array<IAreaSeries>;
+    barSeries?: Array<IBarSeries>;
+    histogramSeries?: Array<IHistogramSeries>;
     width?: number;
     height?: number;
-    options?: object;
+    options?: ChartOptions;
     autoWidth?: boolean;
     autoHeight?: boolean;
     legend?: string;
