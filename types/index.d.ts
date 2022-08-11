c043a7e8a0a99ed81cba56b47ff54b65ee574299
Simon Damberg <damberg.simon00@gmail.com>
2022-08-11 15:47:41 +0200

    Fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,30 @@
-export { default as Chart } from '../src/chart';
+import {
+    MouseEventHandler,
+    TimeRangeChangeEventHandler,
+    IChartApi,
+} from "lightweight-charts";
+import React, { FC } from "react";
+
+export interface IChart extends React.HTMLProps<FC> {
+    candlestickSeries?: Array<any>;
+    lineSeries?: Array<any>;
+    areaSeries?: Array<any>;
+    barSeries?: Array<any>;
+    histogramSeries?: Array<any>;
+    width?: number;
+    height?: number;
+    options?: object;
+    autoWidth?: boolean;
+    autoHeight?: boolean;
+    legend?: string;
+    from?: number;
+    to?: number;
+    onCrosshairMove?: MouseEventHandler;
+    onTimeRangeMove?: TimeRangeChangeEventHandler;
+    darkTheme?: boolean;
+    chartRef?: (chart: IChartApi) => void;
+}
+
+import Chart from "../src";
+export default Chart as IChart;
 export * from "lightweight-charts";
