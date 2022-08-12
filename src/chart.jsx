07d9ae96d1a5e1d5974da21e021aa596a9f4cd2e
Axel Draws <axel.draws@outlook.com>
2022-08-12 09:06:25 +0200

    updated build

--- src/chart.jsx
+++ src/chart.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import { createChart } from "lightweight-charts";
-import equal from "fast-deep-equal";
-import { addSeriesFunctions, colors, defaultDarkTheme, lightTheme } from "./util/consts";
 import usePrevious from "./util/hooks/usePrevious";
-import { mergeDeep } from "./util/helpers";
 
 const Chart = (props) => {
     const { candlestickSeries, lineSeries, areaSeries, barSeries, histogramSeries, width, height, options, autoWidth, autoHeight, legend, from, to, onClick, onCrosshairMove, onTimeRangeMove, darkTheme, chartRef } = props;
 
     const prevProps = usePrevious(props);
-    const [legends, setLegends] = React.useState([]);
-    const [series, setSeries] = React.useState([]);
     const [chart, setChart] = React.useState(null);
     const [color, setColor] = React.useState(lightTheme.layout.textColor);
     const chartDiv = React.createRef();
@@ -18,16 +13,10 @@ const Chart = (props) => {
 
     React.useEffect(() => {
         if (chart) return;
-       const newChart = createChart(chartDiv.current)
-        setChart(newChart)
-        chartRef?.(newChart);
-         
-    }, [chart]);
-  /*   React.useEffect(() => {
-        if (!chart) return;
-        if(chartRef.current){
-        const ls = chartRef?.current?.addLineSeries();
-        ls.setData([
+        const newChart = createChart(chartDiv.current)
+        const ls = newChart.addLineSeries();
+
+         ls.setData([
     { time: '2019-04-11', value: 80.01 },
     { time: '2019-04-12', value: 96.63 },
     { time: '2019-04-13', value: 76.64 },
@@ -39,8 +28,16 @@ const Chart = (props) => {
     { time: '2019-04-19', value: 81.89 },
     { time: '2019-04-20', value: 74.43 },
         ]);
+        setChart(newChart)
+        chartRef?.(newChart);
+         
+    }, [chart]);
+   React.useEffect(() => {
+        if (!chart) return;
+        if(chartRef.current){
+       
             }
-    }, [chartRef.current]); */
+    }, [chartRef.current]); 
 
     return (
         <div ref={chartDiv} style={{ position: "relative" }}>
