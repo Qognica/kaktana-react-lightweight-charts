3bf0ea30dfcbaef799c4bb73f167e62648d7190c
Axel Draws <axel.draws@outlook.com>
2022-08-12 09:11:41 +0200

    Removed whole project

--- src/chart.jsx
+++ src/chart.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { createChart } from "lightweight-charts";
-import usePrevious from "./util/hooks/usePrevious";
-
 const Chart = (props) => {
-    const { candlestickSeries, lineSeries, areaSeries, barSeries, histogramSeries, width, height, options, autoWidth, autoHeight, legend, from, to, onClick, onCrosshairMove, onTimeRangeMove, darkTheme, chartRef } = props;
 
-    const prevProps = usePrevious(props);
-    const [chart, setChart] = React.useState(null);
-    const chartDiv = React.createRef();
-    const legendDiv = React.createRef();
 
 
     return (
-        <div ref={chartDiv} style={{ position: "relative" }}>
+        <div style={{ position: "relative" }}>
             <div
-                ref={legendDiv}
+             
                 style={{
                     position: "absolute",
                     zIndex: 2,
